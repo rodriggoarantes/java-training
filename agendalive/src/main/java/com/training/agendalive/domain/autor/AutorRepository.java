777440681782421347bc339db501be3799708347
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2020-06-08 13:42:22 -0300

    #training #springboot cadastro de agenda com autor

---
+++ agendalive/src/main/java/com/training/agendalive/domain/autor/AutorRepository.java
@@ -0,0 +1,6 @@
+package com.training.agendalive.domain.autor;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AutorRepository extends CrudRepository<Autor, Long> {
+}
