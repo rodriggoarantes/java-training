9a09574b1bcc0e1ba7a6f08ae9d5342db31af2dd
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2020-06-10 18:21:57 -0300

    removendo banco de dados do versionamento

--- agendalive/src/main/java/com/training/agendalive/infra/exception/NotFoundException.java
+++ agendalive/src/main/java/com/training/agendalive/infra/exception/NotFoundException.java
@@ -6,10 +6,10 @@ public class NotFoundException extends BusinessException {
     private static final long serialVersionUID = -2652984264108775089L;
 
     public NotFoundException() {
-        super("PERSONA-0404", "Nenhum resultado encontrado", HttpStatus.NOT_FOUND.value());
+        super("LIVE-0404", "Nenhum resultado encontrado", HttpStatus.NOT_FOUND.value());
     }
 
     public NotFoundException(String detailedMessage) {
-        super("PERSONA-0404", "Nenhum resultado encontrado", detailedMessage, HttpStatus.NOT_FOUND.value());
+        super("LIVE-0404", "Nenhum resultado encontrado", detailedMessage, HttpStatus.NOT_FOUND.value());
     }
 }
