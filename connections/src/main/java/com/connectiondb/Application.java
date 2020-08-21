6c3038eb7d73e75d5166b4ed958bcebda573749a
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2020-08-21 15:19:28 -0300

    teste de conexao com oracle

--- connections/src/main/java/com/connectiondb/Application.java
+++ connections/src/main/java/com/connectiondb/Application.java
@@ -1,9 +1,9 @@
 package com.connectiondb;
 
-import io.vavr.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +13,6 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -22,7 +21,7 @@ public class Application implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
+	private TesteConexao teste;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -31,52 +30,9 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("-------- Oracle JDBC Connection TESTE ------");
-		log.info("-- Working ---");
-
-		final DataSource dataSource = jdbcTemplate.getDataSource();
-		if (dataSource != null) {
-			log.info("Conexao realizada com SUCESSO!");
-
-			final HashSet<Connection> conexoes = new HashSet<>(100);
-			for (int i=0; i < 101; i++) {
-				PreparedStatement prepare = null;
-				ResultSet result = null;
-				try {
-					final Connection connection = dataSource.getConnection();
-					conexoes.add(connection);
-
-					final String sysdate = " SELECT SYSDATE AS NOW FROM DUAL ";
-					prepare = connection.prepareStatement(sysdate);
-					result = prepare.executeQuery();
-					if (result.next()) {
-						log.info("time : {}", result.getTimestamp("NOW"));
-					}
-				} catch (Exception e) {
-					log.error("ERRO", e);
-				} finally {
-					if (prepare != null) {
-						prepare.close();
-					}
-					if (result != null) {
-						result.close();
-					};
-				}
-			}
-
-			log.info("FECHANDO CONEXOES: {}", conexoes.size());
-			final AtomicInteger count = new AtomicInteger(0);
-			conexoes.forEach(conn -> {
-				try {
-					if (count.incrementAndGet() != 100) {
-						conn.close();
-					}
-				} catch (Exception e) {
-					log.error("Falha ao fechar conexao", e);
-				}
-			});
-		}
-
-		log.info("-- Worked ---");
+		teste.testeParalelo();
 		log.info("-------- Oracle JDBC Connection FINALIZADO ------");
 	}
+
+
 }
---
+++ connections/src/main/java/com/connectiondb/TesteConexao.java
@@ -0,0 +1,102 @@
+package com.connectiondb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class TesteConexao {
+    private static final Logger log = LoggerFactory.getLogger(TesteConexao.class);
+
+    private static final HashSet<Connection> CONEXOES = new HashSet<>(200);
+
+    @Value( "${spring.datasource.username}" )
+    private String usernameDb;
+    @Value( "${spring.datasource.hikari.maximumPoolSize}" )
+    private Integer maxConnection;
+    @Value( "${spring.datasource.hikari.connectionTimeout}" )
+    private Long timeout;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void testeParalelo() throws Exception {
+        log.info("-- Working Threads ---");
+        CONEXOES.clear();
+
+        final DataSource dataSource = jdbcTemplate.getDataSource();
+        if (dataSource != null) {
+            log.info("Conexao realizada com SUCESSO!");
+
+            for (int i=0; i < (maxConnection + 1); i++) {
+                final Thread connectionThread = new Thread(() ->
+                        runConnection(dataSource, usernameDb));
+                connectionThread.start();
+            }
+        }
+
+        Thread.sleep((long) (timeout * 1.2));
+        closeAll();
+        log.info("-- Worked Threads ---");
+    }
+
+    private static void runConnection(final DataSource dataSource, final String usernameDb) {
+        PreparedStatement prepare = null;
+        ResultSet result = null;
+        try {
+            Thread.sleep(random());
+            final Connection connection = dataSource.getConnection();
+            CONEXOES.add(connection);
+
+            final String sysdate = " select  count(sid) as CONN, SYSDATE as NOW " +
+                    "from v$session WHERE UPPER(username)='" + usernameDb.toUpperCase() + "' ";
+            prepare = connection.prepareStatement(sysdate);
+            result = prepare.executeQuery();
+            if (result.next()) {
+                log.info("user: {} | time : {} | connections: {}",
+                        usernameDb.toUpperCase(),
+                        result.getTimestamp("NOW"),
+                        result.getInt("CONN"));
+            }
+        } catch (Exception e) {
+            log.error("ERRO ao executar consulta: " + e.getMessage(), e);
+        } finally {
+            if (prepare != null) {
+                try { prepare.close(); }
+                catch (Exception ignored) {}
+            }
+            if (result != null) {
+                try { result.close(); }
+                catch (Exception ignored) {}
+            }
+        }
+    }
+
+    private static void closeAll() {
+        log.info("FECHANDO CONEXOES: {}", CONEXOES.size());
+        final AtomicInteger count = new AtomicInteger(0);
+        CONEXOES.forEach(conn -> {
+            try {
+                    conn.close();
+            } catch (Exception e) {
+                log.error("Falha ao fechar conexao", e);
+            }
+        });
+    }
+
+    private static long random() {
+        final Random rand = new Random();
+        return rand.nextInt(1000);
+    }
+}
